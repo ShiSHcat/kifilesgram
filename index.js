c39611d0050ebed06200bed47bf662b2642f8c13
ShiSHcat <51242428+ShiSHcat@users.noreply.github.com>
2019-11-29 17:01:35 +0100

    gta 5 cinesua hax no malware free

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const tg =require("./stuff/telegram");
 const kf = require("./stuff/kifiles")
 const rg = require("./stuff/register.js")
 const bot = new Telegraf(config.bottoken)
+const a = require("stuff/info.js");
 var users = {};
 bot.on("document",async(ctx)=>tg.doc(ctx,users))
 bot.on("photo",async(ctx)=>await tg.photo(ctx,users))
 bot.on("video",(ctx)=>tg.vid(ctx,users))
 bot.command("login",async(ctx)=>rg(users,ctx))
+bot.command("start",a)
 bot.launch(config.bottoken)
---
+++ stuff/info.js
@@ -0,0 +1,3 @@
+module.exports.start = (ctx)=>{
+    ctx.reply(" Get a KiFiles token on kifiles.gq , do /login [your token here] and then you can forward or send photos and them will be posted to kifiles")
+}
